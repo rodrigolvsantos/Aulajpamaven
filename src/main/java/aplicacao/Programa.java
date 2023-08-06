826cbdb9e5d9a48b8032d2bc7c4bdd589b5693c8
Rodrigo Oliveira <rodrigo.olv.santos@gmail.com>
2023-08-06 19:25:46 -0300

    Project JPA Maven - Find by id

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -11,19 +11,17 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p4 = new Pessoa(null,"Rodrigo Oliveira", "rodrigo.olv.santos@gmail.com");
-		Pessoa p5 = new Pessoa(null,"Naiara Alvez", "naai_alves@gmail.com");
-		Pessoa p6 = new Pessoa(null,"Jose Donizeti", "donizeti@gmail.com");
+		
 			
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(p4);
-		em.persist(p5);
-		em.persist(p6);
-		em.getTransaction().commit();
-		System.out.println("Pronto");
 		
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
+		
+		System.out.println("Pronto");
+		em.close();
+		emf.close();
 		
 	}
 
